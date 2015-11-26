1718e9721359c066b91b990449d86c6495c2a348
Joshua Harris <joshua.jharris13@gmail.com>
2015-11-25 23:02:50 -0500

    made scfApi better, added route for future map view

--- app/controllers/homeController.js
+++ app/controllers/homeController.js
@@ -1,10 +1,21 @@
 var SCFApi = require('../lib/scfApi');
 
 var HomeController = function(server, config) {
+  var scfApi = new SCFApi(config);
   return {
     initRoutes: function() {
+      server.get('/map/issues/:city', function(req, res, next) {
+          // do stuff
+      });
+
+      server.get('/issues/:city', function(req, res, next) {
+        scfApi.getIssuesIn(req.params.city).then(function(data){
+          res.send(data);
+        });
+        next();
+      });
+
       server.get('/places/:city', function(req, res, next) {
-        var scfApi = new SCFApi(config);
         scfApi.getPlacesIn(req.params.city).then(function(data){
           res.send(data);
         });
@@ -12,7 +23,6 @@ var HomeController = function(server, config) {
       });
 
       server.get('/place/:place', function(req, res, next) {
-        var scfApi = new SCFApi(config);
         scfApi.getPlaceDetails(req.params.place).then(function(data){
           res.send(data);
         });
--- app/lib/scfApi.js
+++ app/lib/scfApi.js
@@ -1,45 +1,35 @@
 var request = require('request');
 var promise = require('promise');
 
+function getResponse(url) {
+  return new Promise(function(resolve, reject) {
+    try {
+      var options = {
+        method: 'GET',
+        url: url
+      };
+      request(options, function(err, res, data) {
+        if (err) reject(err);
+        if (res.statusCode !== 200) reject(new Error(res.body));
+        else data = JSON.parse(data);
+        resolve(data);
+      });
+    } catch (apiErr) {
+      reject(apiErr);
+    }
+  });
+}
+
 var SCFApi = function(config) {
   return {
+    getIssuesIn: function(city) {
+      return getResponse(config.apiUrl + 'issues?place_url=' + encodeURIComponent(city));
+    },
     getPlacesIn: function(city) {
-      return new Promise(function(resolve, reject) {
-        try {
-          var url = config.apiUrl + 'places?address=' + encodeURIComponent(city);
-          var options = {
-            method: 'GET',
-            url: url
-          };
-          request(options, function(err, res, data) {
-            if (err) reject(err);
-            if (res.statusCode !== 200) reject(new Error(res.body));
-            else data = JSON.parse(data);
-            resolve(data);
-          });
-        } catch (apiErr) {
-          reject(apiErr);
-        }
-      });
+      return getResponse(config.apiUrl + 'places?address=' + encodeURIComponent(city));
     },
     getPlaceDetails: function(place) {
-      return new Promise(function(resolve, reject) {
-        try {
-          var url = config.apiUrl + 'places/' + encodeURIComponent(place);
-          var options = {
-            method: 'GET',
-            url: url
-          };
-          request(options, function(err, res, data) {
-            if (err) reject(err);
-            if (res.statusCode !== 200) reject(new Error(res.body));
-            else data = JSON.parse(data);
-            resolve(data);
-          });
-        } catch (apiErr) {
-          reject(apiErr);
-        }
-      });
+      return getResponse(config.apiUrl + 'places/' + encodeURIComponent(place));
     }
   }
 };
